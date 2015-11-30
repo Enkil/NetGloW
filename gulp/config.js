ed667e646d9a711534cc14e660597da920567a51
Igor [Enkil] Timohin <timohin.i@gmail.com>
2015-11-30 06:42:34 +0300

    lead block

--- gulp/config.js
+++ gulp/config.js
@@ -38,7 +38,7 @@ module.exports = {
 
     // GitHub Pages options
     ghpOptions: {
-        remoteUrl: "git@github.com:Enkil/amg-webstarter-kit.git"
+        remoteUrl: "git@github.com:Enkil/NetGloW.git"
     },
 
     // jsDoc3 options for JS documentation generating
