192072150ed53c77ace2b9e51a43d566bc297d2f
Igor [Enkil] Timohin <timohin.i@gmail.com>
2016-01-11 10:26:50 +0300

    Update 2016-01-11T07:26:47.064Z

--- js/custom/main.js
+++ js/custom/main.js
@@ -76,5 +76,29 @@ window.onload = function(){
 
     // Google Map
     findMapAndInit('map');
+
+    form = $("form");
+
+    form.each( function() {
+        $(this).validate({});
+    } );
+
+    form.submit (function(event) {
+        if (form.valid())
+        {
+            $('.submission').hide();
+            $('.aftersubmit').show();
+
+            $.ajax({
+                type: form.attr("method"), // use method specified in form attributes
+                url: form.attr("action"), // use action specified in form attributes
+                data: form.serialize(), // encodes set of form elements as string for submission
+                success: function(data) {
+                    // get response from servlet and display on page via jQuery
+                }
+            });
+        }
+        event.preventDefault(); // stop form from redirecting to java servlet page
+    });
 };
 
