72dafb0b4c4844352e83e94c91742db14f4bb578
Igor [Enkil] Timohin <timohin.i@gmail.com>
2016-01-19 20:23:55 +0300

    Update 2016-01-19T17:23:51.846Z

--- js/custom/custom-bundle.js
+++ js/custom/custom-bundle.js
@@ -115,7 +115,7 @@ window.onload = function(){
 
 // Set map
 function initMap(mapIDCurrent) {
-    var myLatlng = new google.maps.LatLng(59.946264, 30.396471); // Marker coordinates
+    var myLatlng = new google.maps.LatLng(59.946262, 30.396538); // Marker coordinates
     var myOptions = { // Map settings
         zoom: 15,
         center: myLatlng,
